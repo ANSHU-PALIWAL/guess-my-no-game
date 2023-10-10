b71390902e48b37148d18575ca22889f47d2d1da
ANSHU-PALIWAL <136322202+ANSHU-PALIWAL@users.noreply.github.com>
2023-10-10 14:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let secreatnumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'no number';
+    }
+
+    else if (guess === secreatnumber) {
+        document.querySelector('.message').textContent = 'correct number';
+
+        document.querySelector('.number').textContent = secreatnumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+
+    else if (guess !== secreatnumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = guess > secreatnumber ? 'too high' : 'too low'; // using ternery opreator to reduce code
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = 'you lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secreatnumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
